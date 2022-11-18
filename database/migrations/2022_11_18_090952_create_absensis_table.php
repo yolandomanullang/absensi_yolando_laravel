4ce74b64d40048954b2e3110b77a0e721d0d0499
Yolando <yolandomanullang@gmail.com>
2022-11-18 16:49:55 +0700

    Membuat Model dan Migration File

---
+++ database/migrations/2022_11_18_090952_create_absensis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsensisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absensis', function (Blueprint $table) {
+            $table->id();
+            $table->time('jam_masuk');
+            $table->time('jam_keluar');
+            $table->integer('tanggal');
+            $table->integer('bulan');
+            $table->integer('tahun');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absensis');
+    }
+}
